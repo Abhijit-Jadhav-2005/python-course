b88b5f97da837c8272106733a9d5fb7d4829b41e
Abhijit-Jadhav-2005 <jadhavabhijit860@gmail.com>
2025-08-07 19:38:32 +0530

    Delete practice set 2 directory

--- practice set 2/prob5.py
+++
@@ -1,9 +0,0 @@
-# write a python program to find an avarage of two numbers entered by the user 
-
-a = int(input("enter any number a :"))
-b = int(input("enter any number b :"))
-
-print(" number 1 :",a)
-print(" number 1 :",b)
-
-print((a+b)/2)
--- practice set 2/prob6.py
+++
@@ -1,5 +0,0 @@
-# write a python program to calculate the square of a number entered by the user.
-
-a = int(input("enter your number :"))
-
-print(a*a)
