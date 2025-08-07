d9a7255fc53ed40dcc77e1c59ceb3516c63e7f15
Abhijit-Jadhav-2005 <jadhavabhijit860@gmail.com>
2025-08-07 19:38:20 +0530

    Delete practice set 1 directory

--- practice set 1/p.set(module)3.py
+++
@@ -1,9 +0,0 @@
-# install an external module and use it to perform an operation of your interest
-
-#  it is a text to speak python module
-
-
-import pyttsx3
-engine = pyttsx3.init()
-engine.say(" shri ganesha")
-engine.runAndWait()
