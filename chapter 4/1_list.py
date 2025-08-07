d65e0df0d07914993e959e08c5b5c6b7827f0aa0
Abhijit-Jadhav-2005 <jadhavabhijit860@gmail.com>
2025-08-07 19:37:53 +0530

    Delete chapter 4 directory

--- chapter 4/1_list.py
+++
@@ -1,6 +0,0 @@
-friends = ["abhijit",23,"apple",5.13,True]
-print(friends[2])
-
-friends[2] = 33
-print(friends[2])
-
--- chapter 4/3_tuple.py
+++
@@ -1,3 +0,0 @@
-a = ( 1, 33, 12, 43, 55, 89, 65, "abhijit", "welcome", 21.55)
-
-print(type(a))
