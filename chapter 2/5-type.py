f8bbed03c6482f63a529e56b090e5a1d48535fbf
Abhijit-Jadhav-2005 <jadhavabhijit860@gmail.com>
2025-08-08 21:49:35 +0530

    Add files via upload

---
+++ chapter 2/5-type.py
@@ -0,0 +1,23 @@
+# class int
+
+a = 21
+t = type(a)
+print(t)
+
+# class float
+
+a = 21.5
+t = type(a)
+print(t)
+
+# class str
+
+a = "abhi"
+t = type(a)
+print(t)
+
+# class bool
+
+f = True&False
+t = type(f)
+print(t)
