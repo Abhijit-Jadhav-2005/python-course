1832620af5523ffc330970a1db77f5068f5a9fd7
Abhijit-Jadhav-2005 <jadhavabhijit860@gmail.com>
2025-08-12 18:44:57 +0530

    Add files via upload

---
+++ practice set 2/prob3.py
@@ -0,0 +1,11 @@
+# check the type of variable assigned without using input() function
+
+a = 22
+t = type(a)
+print(t)
+
+# check the type of variable assigned  using input() function
+
+a = input("enter the the value of a :")
+
+print(type(a))
---
+++ practice set 2/prob4.py
@@ -0,0 +1,15 @@
+# use comparison operator to find out whether a given variable a is greater than b or not , take a = 34 and b = 80
+
+# method 1
+a = 34
+b = 80
+
+print(a>b)
+
+
+# method 2
+
+c = int(input("Enter value for c: "))  
+d = int(input("Enter value for d: "))
+
+print(c > d)
