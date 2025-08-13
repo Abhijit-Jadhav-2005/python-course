da70cbddd8194a24a91eeafca6cc0bf82dc0db98
Abhijit-Jadhav-2005 <jadhavabhijit860@gmail.com>
2025-08-13 18:48:19 +0530

    Add files via upload

---
+++ chapter 3/3_string_fun.py
@@ -0,0 +1,23 @@
+# 1
+len("abhijit")
+print(len("abhijit"))
+
+# 2
+b = "jadhav"
+print(b.endswith("av"))
+
+# 3
+c = "msigff"
+print(c.count("f"))
+
+# 4
+d = "abcdefghi"
+print(d.capitalize())
+
+# 5
+e = "hello world"
+print(e.find("world"))
+
+# 6
+f = "python programming"
+print(f.replace("python", "java"))
