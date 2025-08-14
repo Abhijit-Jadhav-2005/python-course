168a6f675eb1c3f1ef7ab96c8ca6b48f997c61a3
ABHIJIT <jadhavabhijit860@gmail.com>
2025-08-14 21:39:03 +0530

    Add files via upload

---
+++ practice set 3/1_prob.py
@@ -0,0 +1,5 @@
+# write a python program to display a user entered name followed by good afternoon using input () function.
+
+name = input("Enter Your Name : ")
+
+print(f"Good Afternoon, {name}")
---
+++ practice set 3/3_prob.py
@@ -0,0 +1,8 @@
+# write a python program to detect double space in a string.
+
+a = "Hi my self  Abhi"
+
+print(a.find("  "))
+
+# note :- if no space in given string , then output will be (-1)
+# if space is given then output show the position of the space  
